a44a2fbfe983dd0b2f4e2b52fcb3454bff9eab48
Aisuluu Temiralieva <75246512+aiger555@users.noreply.github.com>
2021-08-14 00:17:37 +0600

    Update exam2.py

--- exam2.py
+++ exam2.py
@@ -1,74 +1,83 @@
-class Monitoring:
-    def __init__(self, name, fixed_price, wage, surname, id, hour_worked, hourly_salary, quantity_of_sales, comissions):
-        self.name = name
-        self.fixed_price = fixed_price
-        self.wage = wage + comissions
-        self.comissions = 50*quantity_of_sales
-        self.quantity_of_sales = quantity_of_sales
-        self.surname = surname
+class Employee:
+
+    def __init__(self, id, name, hours):
         self.id = id
-        self.hour_worked = hour_worked
-        self.hourly_salary = hour_worked*100
+        self.name = name
+        self.hours = hours
+        
+    def count_payment(self):
+        pass
 
-    def total_sum(self):
-        total = self.fixed_price + self.wage + self.hourly_salary
-        print(f'{self.fixed_price}-{self.name}-{self.id}')
-        print(f'{self.wage} - {self.name} - {self.id}')
-        print(f'{self.hourly_salary} - {self.name} - {self.id}')
-        return total
-    def best_worker(self):
-        if self.hour_worked > 37:
-            print (f'{self.name} best workers')
-        elif self.hour_worked < 20:
-            print(f'{self.name} lazy worker')
-        else:
-            pass
-
-class Menedjer(Monitoring):
-    def __init__(self, name, fixed_price, surname, id, hour_worked):
-        super().__init__(name, surname, id,  hour_worked, fixed_price)
-    def oplata(self):
-        a = self.hour_worked + self.fixed_price
-        return a
-
-class Secretary(Monitoring):
-    def __init__(self, name, fixed_price,  surname, id, hour_worked):
-        super().__init__(name, surname, id,  hour_worked, fixed_price)
-    def oplata1(self):
-        a1 = self.hour_worked + self.fixed_price
-        return a1
-
-class Secretary_To_Replace(Secretary):
-    def __init__(self, name, hourly_salary, surname, id, hour_worked):
-        super().__init__(name, surname, id,  hour_worked, hourly_salary)
-    def oplata2(self):
-        a_ = self.hour_worked + self.hourly_salary
-        return a_
+class Fixed_salary(Employee):
+    def __init__(self, id, name, hours, amount_of_salary):
+        super().__init__(id, name, hours)
+        self.amount_of_salary = amount_of_salary
+    
+    def count_payment(self):
+        return self.amount_of_salary
 
 
-class Seller(Monitoring):
-    def __init__(self, name, wage, comissions, surname, id, hour_worked, quantity_of_sales):
-        super().__init__(name, surname, id,  hour_worked, wage, comissions, quantity_of_sales)
-    def oplata3(self):
-        a2 = self.hour_worked + self.wage
-        return a2
+class Comission(Employee):
+    def __init__(self, id, name, hours, amount_of_salary, amount_of_sales):
+        super().__init__(id, name, hours)
+        self.amount_of_salary = amount_of_salary
+        self.amount_of_sales = amount_of_sales
 
-class Department_worker(Monitoring):
-    def __init__(self, name, hourly_salary, surname, id, hour_worked):
-        super().__init__(name, surname, id, hour_worked, hourly_salary)
-    def oplata4(self):
-        a3 = self.hour_worked + self.hourly_salary
-        return a3
+    def count_payment(self):
+        return self.amount_of_salary + self.amount_of_sales * 50
 
-m = Menedjer('Барсбек', 45000, 'Канаткулов', 1, 18)
-m.oplata()
-s = Secretary('Алымкул', 20000, 'Тилекбаев', 2, 38)
-s.oplata1()
-ss = Secretary_To_Replace('Жанар', 18000, 'Рыскулов', 6, 33)
-ss.oplata2()
-c = Seller('Айпери', 20000, 50, 'Шалымбекова', 3, 38, 20)
-c.oplata3()
-d = Department_worker('Бакыт', 20000, 'Рустамов', 4, 25)
-d.oplata4()
-d1 = Department_worker('Алтынай', 20000, 'Ширинбаева', 5, 40)
-d1.oplata4()
+    
+class Hourly_Paid(Employee):
+
+    def __init__(self, id, name, amount_of_salary, hours):
+        super().__init__(id, name, hours)
+        self.amount_of_salary = amount_of_salary
+   
+    def count_payment(self):
+        return self.hours * 100
+
+
+def calculate_all_salaries(employees):
+    total_sum = 0
+    for employee in employees:
+        salary = employee.count_payment()
+        print('ID-', employee.id, 'NAME:', employee.name, 'SALARY:',employee.amount_of_salary)
+        total_sum += salary
+
+    print('Sum that spent to workers', total_sum)
+
+def productive_w(list_of_productivity, list_of_names):
+    most_productivity, most_productive_person = 0, None
+    least_productivity, least_productive_person = 100, None
+    for i in range(len(list_of_productivity) - 1):
+        if list_of_productivity[i] > most_productivity:
+            most_productivity, most_productive_person = list_of_productivity[i], \
+                list_of_names[i]
+        if list_of_productivity[i] < least_productivity:
+            least_productivity, least_productive_person = list_of_productivity[i], \
+                list_of_names[i]
+
+    print(f'Most productive {most_productive_person} his productivity {most_productivity}')
+    print(f'Least productive {least_productive_person} his productivity {least_productivity}')
+
+
+def calculate_productivity(employees):
+    hours_in_week = 40
+    list_of_productivity = []
+    list_of_names = []
+    for employee in employees:
+        productivity = employee.hours / hours_in_week * 100
+        print(employee.name, productivity)
+        list_of_productivity.append(productivity)
+        list_of_names.append(employee.name)
+    productive_w(list_of_productivity, list_of_names)
+
+manager = Fixed_salary(id=1, name='Jake', amount_of_salary=50000, hours=16)
+secretary = Fixed_salary(id=2, name='Lili', amount_of_salary=60000, hours=20)
+saler = Comission(id=3, name='Rose', amount_of_salary=40000, amount_of_sales=25, hours = 10)
+worker = Hourly_Paid(id=4, name='Dora', amount_of_salary=45000, hours = 12)
+secretary_for_change = Hourly_Paid(id=5, name='Dim', amount_of_salary=43000, hours = 10)
+
+list_of_workers = [manager, secretary, saler, worker, secretary_for_change]
+calculate_all_salaries(list_of_workers)
+calculate_productivity(list_of_workers)
